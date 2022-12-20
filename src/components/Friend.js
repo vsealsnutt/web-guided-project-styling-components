742e552da0220a605a646564252c4c29717a84c5
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2022-12-20 10:45:07 -0800

    followed along with the guided project video to build and style a friends list with friend details

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -1,12 +1,63 @@
 import React from 'react'
+import styled, { keyframes } from 'styled-components';
 
-export default function Friend({ info, action }) {
+/*
+.friend {
+  width: 60%;
+  display: flex;
+  justify-content: space-between;
+}
+*/
+
+const kf = keyframes`
+  50% {
+    transform: scale(0.8);
+  }
+  
+  100% {
+    transform: scale(1);
+    opacity: 1;
+  }`;
+
+const StyledFriend = styled.div`
+  width: 60%;
+  display: flex;
+  justify-content: space-between; 
+  padding: 8px;
+  border-bottom: 2px solid white;
+
+  background-color: ${pr => pr.theme.primaryColor};
+  color: ${pr => pr.theme.white};
+
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background-color: ${pr => pr.theme.secondaryColor};
+  }
+
+  &::before {
+    content: '${pr => pr.besty ? '💚' : '😀'}';
+  }
+
+  button {
+    background-color: ${pr => pr.theme.tertiaryColor};
+    &:hover {
+      transform: scale(1.2);
+    }
+  }
+
+  transform: scale(2);
+  opacity: 0;
+  animation: ${kf} 0.3s ease-in-out forwards;
+`;
+
+export default function Friend({ info, action, besty }) {
   return (
-    <div className='friend'>
+    <StyledFriend besty={besty}>
       {info.name}
       <button onClick={() => action(info.id)}>
         See details
       </button>
-    </div>
+    </StyledFriend>
   )
 }
